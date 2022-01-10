bec1d4a1137798e2c22f13d9ca556401c4397fdf
alexisdacosta <pro.alexisdacosta@gmail.com>
2022-01-10 09:08:12 +0100

    modification demo

--- demo/demo_characters_CNN.py
+++ demo/demo_characters_CNN.py
@@ -25,16 +25,9 @@ def classify(input):
     pred = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A' ,'B', 'C', 'D', 'E', 'F', 'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','d','e','f','g','h','n','q','r','t']
 
     pre = pred[np.argmax(model.predict(img_6))]
-    print(pre)
     
-    prediction = model.predict(img_6).tolist()[0]
-    
-    for p in prediction:
-        p = str(pred[int(p)])
-        
-    return {str(i): prediction[i] for i in range(10)}
+    return pre
 
 label = gr.outputs.Label(num_top_classes=3)
-interface = gr.Interface(fn=classify, inputs="sketchpad", outputs=label, 
-live=False)
+interface = gr.Interface(fn=classify, inputs="sketchpad",  outputs="text", live=False)
 interface.launch()
